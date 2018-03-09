1b34e3f152ebd8e243039f4483886da037ec126b
陈颂颂 <chensongsong@geetest.com>
2018-03-09 18:32:55 +0800

    add testbutton V3.5.1 and onepass V1.3.5

--- app/src/main/java/com/example/geeonepassdemo/MainActivity.java
+++ app/src/main/java/com/example/geeonepassdemo/MainActivity.java
@@ -9,13 +9,11 @@ import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.net.ConnectivityManager;
-import android.os.AsyncTask;
+import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -24,12 +22,10 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
-import com.geetest.gt3unbindsdk.Bind.GT3GeetestBindListener;
-import com.geetest.gt3unbindsdk.Bind.GT3GeetestUtilsBind;
 import com.geetest.onepass.BaseGOPListener;
 import com.geetest.onepass.GOPGeetestUtils;
-
+import com.geetest.sdk.Bind.GT3GeetestBindListener;
+import com.geetest.sdk.Bind.GT3GeetestUtilsBind;
 
 import org.json.JSONException;
 import org.json.JSONObject;
